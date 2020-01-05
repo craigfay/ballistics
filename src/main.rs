2fc73fc85192e4cab8a568b13cacfb93c5b4f28e
craig fay <craigfay1@gmail.com>
2020-01-05 12:04:13 -0600

    Comment

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ fn main() {
 
 type Kilometers = f64;
 
+// The gravitational force is propoportional to 1/R^2, where R is the distance
+// to the center of the Earth.
 fn gravitational_force(altitude: Kilometers) -> f64 {
     let equator_to_core: Kilometers = 6400.0;
     let distance: Kilometers = altitude + equator_to_core;
