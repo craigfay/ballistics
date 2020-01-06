db1fe8a0df8768d2a50f13ead25eea3d55c45586
craig fay <craigfay1@gmail.com>
2020-01-05 18:32:09 -0600

    Correct gravitational force formula

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,10 @@ type Kilometers = f64;
 // The gravitational force is propoportional to 1/R^2, where R is the distance
 // to the center of the Earth.
 fn gravitational_force(altitude: Kilometers) -> f64 {
+    let gravitational_force_at_equator = 9.8;
     let equator_to_core: Kilometers = 6400.0;
     let distance: Kilometers = altitude + equator_to_core;
-
-    1.0 / distance.powf(2.0)
+    gravitational_force_at_equator / (distance / equator_to_core).powf(2.0)
 }
+
+
