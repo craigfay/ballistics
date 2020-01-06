278c99524dab322fe11acf48cce05f2084deabdb
craig fay <craigfay1@gmail.com>
2020-01-05 18:35:59 -0600

    Increase variable accuracy

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,13 @@ fn main() {
 }
 
 type Kilometers = f64;
+type MetersPerSecondSquared = f64;
 
 // The gravitational force is propoportional to 1/R^2, where R is the distance
 // to the center of the Earth.
-fn gravitational_force(altitude: Kilometers) -> f64 {
-    let gravitational_force_at_equator = 9.8;
-    let equator_to_core: Kilometers = 6400.0;
+fn gravitational_force(altitude: Kilometers) -> MetersPerSecondSquared {
+    let gravitational_force_at_equator = 9.798;
+    let equator_to_core: Kilometers = 6378.0;
     let distance: Kilometers = altitude + equator_to_core;
     gravitational_force_at_equator / (distance / equator_to_core).powf(2.0)
 }
-
-
