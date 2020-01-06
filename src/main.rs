4d273c9f5eb052277c097bfa289601441b376c4d
craig fay <craigfay1@gmail.com>
2020-01-05 18:37:45 -0600

    Type Rename

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ fn main() {
 }
 
 type Kilometers = f64;
-type MetersPerSecondSquared = f64;
+type MetersPerSecSquared = f64;
 
 // The gravitational force is propoportional to 1/R^2, where R is the distance
 // to the center of the Earth.
-fn gravitational_force(altitude: Kilometers) -> MetersPerSecondSquared {
-    let gravitational_force_at_equator = 9.798;
+fn gravitational_force(altitude: Kilometers) -> MetersPerSecSquared {
+    let gravitational_force_at_equator: MetersPerSecSquared = 9.798;
     let equator_to_core: Kilometers = 6378.0;
     let distance: Kilometers = altitude + equator_to_core;
     gravitational_force_at_equator / (distance / equator_to_core).powf(2.0)
